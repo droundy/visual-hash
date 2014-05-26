0949dd975abad96d544f4eacbb6aefa97ae168bf
David Roundy <roundyd@physics.oregonstate.edu>
2014-05-26 08:48:29 -0700

    switch to using structs internally and add "parts" output

--- FractalPython.py
+++ FractalPython.py
@@ -7,19 +7,21 @@ import random
 import pyximport; pyximport.install()
 import FractalTransform
 
-a = FractalTransform.CMultiple()
+a = FractalTransform.Multiple()
+parts = a.TakeApart()
 
 size = 128
-h = FractalTransform.Simulate(a, FractalTransform.MakePoint(.1,.232332), size, size)
 
-img = Image.new( 'RGB', (size,size), "black") # create a new black image
-pixels = img.load() # create the pixel map
+for filename, transform in parts:
+    h = FractalTransform.Simulate(transform, FractalTransform.MakePoint(.1,.232332), size, size)
+    img = Image.new( 'RGB', (size,size), "black") # create a new black image
+    pixels = img.load() # create the pixel map
+    colors = FractalTransform.get_colors(h)
 
-colors = FractalTransform.get_colors(h)
+    for i in range(img.size[0]):    # for every pixel:
+        for j in range(img.size[1]):
+            pixels[i,j] = (int(256*colors[0,i,j]), int(256*colors[1,i,j]), int(256*colors[2,i,j])) # set the colour accordingly
 
-for i in range(img.size[0]):    # for every pixel:
-    for j in range(img.size[1]):
-        pixels[i,j] = (int(256*colors[0,i,j]), int(256*colors[1,i,j]), int(256*colors[2,i,j])) # set the colour accordingly
-
-img.save('image.png')
+    img.save(filename)
 #img.show()
+
--- FractalTransform.pyx
+++ FractalTransform.pyx
@@ -25,178 +25,181 @@ def MakePoint(x, y):
     cdef Point p = Point(x, y, 0,0,0,0)
     return p
 
-cdef class Transform:
-    cdef Point transform(self, Point p):
-        return Point(0, 0, 0, 0, 0, 0)
-    cdef double R, G, B, A
-    def __call__(self, p):
-        return self.transform(p)
+cdef struct ColorTransform:
+    double R, G, B, A
+
+cdef ColorTransform MakeColorTransform():
+    cdef ColorTransform out
+    out.A = 1
+    out.R = random.uniform(0,1)
+    out.G = random.uniform(0,1)
+    out.B = random.uniform(0,1)
+    cdef double m = out.R
+    if out.G > m:
+        m = out.G
+    if out.B > m:
+        m = out.B
+    out.R /= m
+    out.G /= m
+    out.B /= m
+    return out
+
+cdef Point colorTransform(ColorTransform c, Point p):
+    p.R = (c.A*c.R + p.A*p.R)/(c.A + p.A)
+    p.G = (c.A*c.G + p.A*p.G)/(c.A + p.A)
+    p.B = (c.A*c.B + p.A*p.B)/(c.A + p.A)
+    p.A = (c.A+p.A)/2
+    return p
+
+cdef struct Affine:
+    ColorTransform c
+    double compressme, theta
+    double Mxx, Mxy, Myx, Myy, Ox, Oy
+
+cdef Affine MakeAffine():
+    cdef Affine a
+    a.c = MakeColorTransform()
+    # currently we always initialize pseudorandomly, but
+    # eventually we'll want to generate this deterministically.
+    a.theta = random.uniform(0, 2*np.pi)
+    rot = np.matrix([[cos(a.theta), sin(a.theta)],
+                     [-sin(a.theta),cos(a.theta)]])
+    a.compressme = random.gauss(0.8, 0.2)
+    compress = np.matrix([[a.compressme, 0],
+                          [0, a.compressme]])
+    mat = compress*rot
+    a.Mxx = mat[0,0]
+    a.Mxy = mat[0,1]
+    a.Myx = mat[1,0]
+    a.Myy = mat[1,1]
+    cdef double translation_scale = 0.8
+    a.Ox = random.gauss(0, translation_scale)
+    a.Oy = random.gauss(0, translation_scale)
+    return a
+
+cdef Point affineTransform(Affine a, Point p):
+    cdef Point out = colorTransform(a.c, p)
+    p.x -= a.Ox
+    p.y -= a.Oy
+    out.x = p.x*a.Mxx + p.y*a.Mxy # + a.Ox
+    out.y = p.x*a.Myx + p.y*a.Myy # + a.Oy
+    #p.x += a.Ox
+    #p.y += a.Oy
+    return out
+
+cdef struct Fancy:
+    Affine a
+    double spiralness, radius, bounciness
+    int bumps
+
+cdef Fancy MakeFancy():
+    cdef Fancy f
+    f.a = MakeAffine()
+    f.spiralness = random.gauss(0, 3)
+    f.radius = random.gauss(sqrt(2)/2, sqrt(2)/2/2)
+    f.bounciness = random.gauss(2, 2)
+    f.bumps = random.randint(1, 4)
+    return f
+
+cdef Point fancyTransform(Fancy f, Point p):
+    cdef Point out = affineTransform(f.a, p)
+    cdef Point nex = out
+    cdef double r = sqrt(out.x*out.x + out.y*out.y)
+    cdef double theta = atan2(out.y, out.x)
+    cdef double maxrad = f.radius*(1+f.bounciness*sin(theta*f.bumps))
+    nex.x = maxrad*sin(r/maxrad)*sin(theta + f.spiralness*r)
+    nex.y = maxrad*sin(r/maxrad)*cos(theta + f.spiralness*r)
+    nex.x = sin(nex.x)
+    nex.y = sin(nex.y)
+    return nex
+
+def fancyFilename(Fancy f):
+    return 'image_%.2g_%.2g_%.2g_%d__%.2g_%.2g.png' % (f.radius, f.spiralness, f.bounciness, f.bumps, f.a.compressme, f.a.theta)
+
+cdef struct Symmetry:
+    Affine a
+    int Nsym
+
+cdef Symmetry MakeSymmetry():
+    cdef Symmetry s
+    s.a = MakeAffine()
+    cdef double theta = random.uniform(0, 2*np.pi)
+    s.a.Ox /= 3
+    s.a.Oy /= 3
+    cdef double nnn = random.expovariate(1.0/4)
+    s.Nsym = 1 + <int>nnn
+    if s.Nsym == 1 and random.randint(0,1) == 0:
+        print 'Mirror plane'
+        s.Nsym = 2
+        theta = random.uniform(0, 2*np.pi)
+        vx = sin(theta)
+        vy = cos(theta)
+        s.a.Mxx = vx
+        s.a.Myy = -vx
+        s.a.Mxy = vy
+        s.a.Myx = vy
+    else:
+        print 'Rotation:', s.Nsym, 'from', nnn
+        s.a.Mxx = cos(2*np.pi/s.Nsym)
+        s.a.Myy = s.a.Mxx
+        s.a.Mxy = sin(2*np.pi/s.Nsym)
+        s.a.Myx = -s.a.Mxy
+    print np.array([[s.a.Mxx, s.a.Mxy],
+                    [s.a.Myx, s.a.Myy]])
+    print 'origin', s.a.Ox, s.a.Oy
+    return s
+
+cdef Point symmetryTransform(Symmetry s, Point p):
+    cdef double px = p.x
+    cdef double py = p.y
+    px -= s.a.Ox
+    py -= s.a.Oy
+    p.x = px*s.a.Mxx + py*s.a.Mxy
+    p.y = px*s.a.Myx + py*s.a.Myy
+    p.x += s.a.Ox
+    p.y += s.a.Oy
+    return p
+
+cdef int Ntransform = 4
+cdef struct CMultiple:
+    Fancy t[10]
+    Symmetry s
+    int N
+    int Ntot
+
+cdef CMultiple MakeMultiple():
+    cdef CMultiple m
+    m.s = MakeSymmetry()
+    m.N = Ntransform - m.s.Nsym
+    if m.N < 4:
+        m.N = 4
+    cdef int i
+    for i in range(m.N):
+        m.t[i] = MakeFancy()
+    m.Ntot = m.N*m.s.Nsym
+    return m
+
+cdef Point multipleTransform(CMultiple m, Point p):
+    cdef int i = rand() % m.Ntot
+    if i < m.N:
+        return fancyTransform(m.t[i], p)
+    return symmetryTransform(m.s, p)
+
+cdef class Multiple:
+    cdef CMultiple m
     def __cinit__(self):
-        self.A = 1
-        self.R = random.uniform(0,1)
-        self.G = random.uniform(0,1)
-        self.B = random.uniform(0,1)
-        cdef double m = self.R
-        if self.G > m:
-            m = self.G
-        if self.B > m:
-            m = self.B
-        self.R /= m
-        self.G /= m
-        self.B /= m
-    cdef Point colortransform(self, Point p):
-        p.R = (self.A*self.R + p.A*p.R)/(self.A + p.A)
-        p.G = (self.A*self.G + p.A*p.G)/(self.A + p.A)
-        p.B = (self.A*self.B + p.A*p.B)/(self.A + p.A)
-        p.A = (self.A+p.A)/2
-        return p
-
-cdef class Affine(Transform):
-    cdef public double Mxx, Mxy, Myx, Myy, Ox, Oy
-    cdef public Point O
-    def __cinit__(self):
-        # currently we always initialize pseudorandomly, but
-        # eventually we'll want to generate this deterministically.
-        cdef double theta = random.uniform(0, 2*np.pi)
-        rot = np.matrix([[cos(theta), sin(theta)],
-                         [-sin(theta),cos(theta)]])
-        cdef double compressme = random.gauss(0.8, 0.2)
-        compress = np.matrix([[compressme, 0],
-                              [0, compressme]])
-        mat = compress*rot
-        self.Mxx = mat[0,0]
-        self.Mxy = mat[0,1]
-        self.Myx = mat[1,0]
-        self.Myy = mat[1,1]
-        cdef double translation_scale = 0.8
-        self.Ox = random.gauss(0, translation_scale)
-        self.Oy = random.gauss(0, translation_scale)
-    cdef Point transform(self, Point p):
-        cdef Point out = self.colortransform(p)
-        p.x -= self.Ox
-        p.y -= self.Oy
-        out.x = p.x*self.Mxx + p.y*self.Mxy # + self.Ox
-        out.y = p.x*self.Myx + p.y*self.Myy # + self.Oy
-        #p.x += self.Ox
-        #p.y += self.Oy
-        return out
-    def __str__(self):
-        return 'M='+str(((self.Mxx, self.Mxy), (self.Myx, self.Myy))) + ' O='+str(self.O) + ' C=%g, %g, %g, %g' % (self.R, self.G, self.B, self.A)
-
-
-cdef class Fancy(Affine):
-    cdef double spiralness, radius
-    def __cinit__(self):
-        self.spiralness = random.gauss(0, 3)
-        self.radius = random.gauss(sqrt(2), sqrt(2)/2)
-    cdef Point transform(self, Point p):
-        cdef Point out = Affine.transform(self,p)
-        cdef Point nex = out
-        cdef double r = sqrt(out.x*out.x + out.y*out.y)
-        cdef double theta = atan2(out.y, out.x)
-        nex.x = self.radius*sin(r/self.radius)*sin(theta + self.spiralness*r)
-        nex.y = self.radius*sin(r/self.radius)*cos(theta + self.spiralness*r)
-        nex.x = sin(nex.x)
-        nex.y = sin(nex.y)
-        return nex
-
-cdef class Symmetry(Affine):
-    cdef int Nsym
-    def __cinit__(self):
-        cdef double theta = random.uniform(0, 2*np.pi)
-        self.Ox /= 3
-        self.Oy /= 3
-        cdef double nnn = random.expovariate(1.0/4)
-        self.Nsym = 1 + <int>nnn
-        if self.Nsym == 1 and random.randint(0,1) == 0:
-            print 'Mirror plane'
-            self.Nsym = 2
-            theta = random.uniform(0, 2*np.pi)
-            vx = sin(theta)
-            vy = cos(theta)
-            self.Mxx = vx
-            self.Myy = -vx
-            self.Mxy = vy
-            self.Myx = vy
-        else:
-            print 'Rotation:', self.Nsym, 'from', nnn
-            self.Mxx = cos(2*np.pi/self.Nsym)
-            self.Myy = self.Mxx
-            self.Mxy = sin(2*np.pi/self.Nsym)
-            self.Myx = -self.Mxy
-        print np.array([[self.Mxx, self.Mxy],
-                        [self.Myx, self.Myy]])
-        print 'origin', self.Ox, self.Oy
-    cdef Point transform(self, Point p):
-        cdef double px = p.x
-        cdef double py = p.y
-        px -= self.Ox
-        py -= self.Oy
-        p.x = px*self.Mxx + py*self.Mxy
-        p.y = px*self.Myx + py*self.Myy
-        p.x += self.Ox
-        p.y += self.Oy
-        return p
-
-cdef int Ntransform = 10
-class Multiple(Transform):
-    def __init__(self):
-        symm = Symmetry()
-        self.N = Ntransform - symm.Nsym
-        if self.N < 4:
-            self.N = 4
-        self.t = [1]*self.N
-        for i in xrange(self.N):
-            self.t[i] = Fancy()
-        if symm.Nsym > 1:
-            self.t = self.t + [symm]*((symm.Nsym-1)*self.N)
-    def transform(self, p):
-        return self.t[rand() % len(self.t)].transform(p)
-
-cdef class CMultiple(Transform):
-    cdef Transform a, b, c, d, e, f, g, h, i, j
-    cdef Symmetry symm
-    cdef int N, Ntot
-    def __init__(self):
-        print 'using CMultiple'
-        self.symm = Symmetry()
-        self.N = Ntransform - self.symm.Nsym
-        if self.N < 4:
-            self.N = 4
-        self.Ntot = self.symm.Nsym*self.N
-        self.a = Fancy()
-        self.b = Fancy()
-        self.c = Fancy()
-        self.d = Fancy()
-        self.e = Fancy()
-        self.f = Fancy()
-        self.g = Fancy()
-        self.h = Fancy()
-        self.i = Fancy()
-        self.j = Fancy()
-    cdef Point transform(self, Point p):
-        cdef int n = rand() % self.Ntot
-        if n >= self.N:
-            return self.symm.transform(p)
-        if n == 1:
-            return self.b.transform(p)
-        elif n == 2:
-            return self.c.transform(p)
-        elif n == 3:
-            return self.d.transform(p)
-        elif n == 4:
-            return self.e.transform(p)
-        elif n == 5:
-            return self.f.transform(p)
-        elif n == 6:
-            return self.g.transform(p)
-        elif n == 7:
-            return self.h.transform(p)
-        elif n == 8:
-            return self.i.transform(p)
-        elif n == 9:
-            return self.j.transform(p)
-        return self.a.transform(p)
+        self.m = MakeMultiple()
+    cpdef Point transform(self, Point p):
+        return multipleTransform(self.m, p)
+    def TakeApart(self):
+        transforms = [('image.png', self)]
+        for i in range(self.m.N):
+            foo = Multiple()
+            foo.m.N = 1
+            foo.m.Ntot = 1
+            foo.m.t[0] = self.m.t[i]
+            transforms.append((fancyFilename(foo.m.t[0]), foo))
+        return transforms
 
 cdef place_point(np.ndarray[DTYPE_t, ndim=3] h, Point p):
     cdef int ix = <int>((p.x+1)/2*h.shape[1])
@@ -206,12 +209,15 @@ cdef place_point(np.ndarray[DTYPE_t, ndim=3] h, Point p):
     h[2, ix % h.shape[1], iy % h.shape[2]] += p.G
     h[3, ix % h.shape[1], iy % h.shape[2]] += p.B
 
-cpdef np.ndarray[DTYPE_t, ndim=3] Simulate(CMultiple t, Point p,
+cpdef np.ndarray[DTYPE_t, ndim=3] Simulate(Multiple t, Point p,
                                            int nx, int ny):
     cdef np.ndarray[DTYPE_t, ndim=3] h = np.zeros([4, nx,ny], dtype=DTYPE)
+    if t.m.Ntot == 0:
+        print 'weird business'
+        return h
     for i in xrange(400*nx*ny):
         place_point(h, p)
-        p = t.transform(p)
+        p = multipleTransform(t.m, p)
     return h
 
 cpdef np.ndarray[DTYPE_t, ndim=3] get_colors(np.ndarray[DTYPE_t, ndim=3] h):
