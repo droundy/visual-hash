0185c858c28494399dde244c0035165b404d0b46
David Roundy <roundyd@physics.oregonstate.edu>
2014-06-10 17:48:15 -0700

    print timing comparison of new versus old code

--- hash.py
+++ hash.py
@@ -16,19 +16,28 @@ use = 'old'
 use = 'new'
 use = 'both'
 
+timereport = ''
+
 if use == 'old' or use == 'both':
     print '\nworking on old fractal'
     print '======================'
+    before = time.clock()
     img = VisualHash.OldFractal(VisualHash.StrongRandom(data), 128)
+    after = time.clock()
+    timereport += '\nold fractal algorithm took %g seconds' % (after - before)
     img.save('oldimage.png')
 
 if use == 'new' or use == 'both':
     print '\nworking on new fractal'
     print '======================'
+    before = time.clock()
     img = myhash(VisualHash.StrongRandom(data), 128)
+    after = time.clock()
+    timereport += '\nnew fractal algorithm took %g seconds' % (after - before)
     img.save('image.png')
 #img.show()
 
+print timereport
 
 # for i in range(10):
 #     print "\n==========", i
