eec895f6407e37ad9f358f89d3169d9cc26f48de
David Roundy <roundyd@physics.oregonstate.edu>
2014-09-11 13:26:14 -0700

    remove old todo list
    Tested on bennet.
    
    optimized fractal algorithm took 20.45 seconds

--- statistics/predict_data.py
+++ statistics/predict_data.py
@@ -14,18 +14,6 @@ N = 1#the constants are given float values here
 q = 1
 A = 0
 
-# TO DO!  :)
-
-to_do = [""" Write a function to simulate measurements, which is to say, given a set of
-             f values and q, A and N, return a set of 0s and 1s indicating whether it
-             remained the same.  Using random number generator.""",
-         """ Write a function that given a bunch of measurement results and a q, A, N,
-             will return the "probability" of finding this set of measurements. """,
-         """ (later) Think about eliminating q or N in favor of entropy. """,
-         """ (later) Visualize the probability above, how it depends on q, A, and N,
-             to show what combinations are likely. """,
-         """ (later) Choose next f based on previous observations. """]
-
 p = np.zeros_like(random_data.f) # the probability of staying the same for each f value
 for i in range(len(random_data.f)):
     p[i] = random_data.Prob(random_data.f[i])
